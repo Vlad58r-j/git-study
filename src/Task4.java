fc2bfeb8f5f4f4cc695bd14bbb6a43d3501ca130
Vlad Krivonos <vlad58r@gmail.com>
2026-02-10 11:46:59 +0300

    Edit Task4

--- src/Task4.java
+++ src/Task4.java
@@ -7,25 +7,56 @@ public class Task4 {
         System.out.print("Размер массива -> ");
         var arrayLength = new Scanner(System.in).nextInt();
         int[] array = new int[arrayLength];
+        var counterNegative = 0;
         for (int i = 0; i < arrayLength; i++) {
-            array[i] = new Random().nextInt(101);
+            array[i] = new Random().nextInt(201) - 100;
+            counterNegative = array[i] < 0 ? counterNegative++ : counterNegative;
         }
         System.out.println(Arrays.toString(array));
 
         var result = 1;
-        for (int i = 0; i < 3; i++) {
-            var max = 0;
-            var position = 0;
-
+        var multi = 0;
+        for (int i = 0; i < arrayLength; i++) {
             for (int j = 0; j < arrayLength; j++) {
-                if (array[j] > max) {
-                    max = array[j];
-                    position = j;
+                for (int k = 0; k < arrayLength; k++) {
+                    if (k != i && i != j && k != j) {
+                        multi = array[i] * array[j] * array[k];
+                        result = Math.max(multi, result);
+                    }
+
                 }
             }
-            result *= max;
-            array[position] = 0;
         }
+//        for (int i = 0; i < 3; i++) {
+//            var max = 0;
+//            var position = 0;
+//            var positionMin = 0;
+//            var min = 0;
+//
+//            if (counterNegative > 1)
+//                for (int j = 0; j < arrayLength; j++) {
+//                    if (array[j] < min) {
+//                        min = array[j];
+//                        positionMin = j;
+//                    }
+//                }
+//            array[positionMin] = 0;
+//
+//
+//
+//            position = 0;
+//            for (int j = 0; j < arrayLength; j++) {
+//                if (array[j] > max) {
+//                    if (Math.abs(array[j]) > max) {
+//
+//                    }
+//                    max = array[j];
+//                    position = j;
+//                }
+//            }
+//            result *= max;
+//            array[position] = 0;
+//        }
 
         System.out.println(result);
     }
